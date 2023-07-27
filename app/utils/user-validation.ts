f05c8f6a0fd081d83b4a3bdb7244d7a669bdda6e
André Casal <andre@andrecasal.com>
2023-07-27 16:37:07 +0100

    add newsletter form

--- app/utils/user-validation.ts
+++ app/utils/user-validation.ts
@@ -8,16 +8,6 @@ export const usernameSchema = z
 		message: 'Username can only include letters, numbers, and underscores',
 	})
 
-export const passwordSchema = z
-	.string()
-	.min(6, { message: 'Password is too short' })
-	.max(100, { message: 'Password is too long' })
-export const nameSchema = z
-	.string()
-	.min(3, { message: 'Name is too short' })
-	.max(40, { message: 'Name is too long' })
-export const emailSchema = z
-	.string()
-	.email({ message: 'Email is invalid' })
-	.min(3, { message: 'Email is too short' })
-	.max(100, { message: 'Email is too long' })
+export const passwordSchema = z.string().min(6, { message: 'Password is too short' }).max(100, { message: 'Password is too long' })
+export const nameSchema = z.string().min(2, { message: 'Name is too short' }).max(40, { message: 'Name is too long' })
+export const emailSchema = z.string().email({ message: 'Email is invalid' }).min(3, { message: 'Email is too short' }).max(100, { message: 'Email is too long' })
