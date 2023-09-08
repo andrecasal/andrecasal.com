31b42936688dbd6768c5a1787fd887d493366f57
André Casal <andre@andrecasal.com>
2023-09-08 22:07:42 +0100

    add installation instructions

--- app/routes/_marketing+/ui+/installation.tsx
+++ app/routes/_marketing+/ui+/installation.tsx
@@ -7,7 +7,10 @@ const Installation = () => {
 				Installation
 			</Text>
 			<Text size="md" className="mt-2">
-				Coming soon 😄 🎉
+				Installation is straightforward, even if a bit cumbersome for now: copy and paste all the components into their own files.
+			</Text>
+			<Text size="md" className="mt-2">
+				I'll open source my website soon, so you can more easily copy+paste all components at once, and see how I use the library. I'll also create a CLI to make this process easier.
 			</Text>
 		</>
 	)
