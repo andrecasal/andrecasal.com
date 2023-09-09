5daf2311346b1490c961ee4fe46708374b52f00c
André Casal <andre@andrecasal.com>
2023-09-09 19:49:44 +0100

    add fluid text size to heading

--- app/routes/_marketing+/ui+/components/typography/heading.tsx
+++ app/routes/_marketing+/ui+/components/typography/heading.tsx
@@ -4,16 +4,26 @@ import { cn } from '~/utils/misc.ts'
 
 const headingVariants = cva('', {
 	variants: {
+		size: {
+			'4xl': 'text-size-4xl',
+			'3xl': 'text-size-3xl',
+			'2xl': 'text-size-2xl',
+			xl: 'text-size-xl',
+			lg: 'text-size-lg',
+			md: 'text-size-md',
+			sm: 'text-size-sm',
+			xs: 'text-size-xs',
+		},
 		weight: {
-			thin: 'font-thin',
-			extralight: 'font-extralight',
-			light: 'font-light',
-			normal: 'font-normal',
-			medium: 'font-medium',
-			semibold: 'font-semibold',
-			bold: 'font-bold',
-			extrabold: 'font-extrabold',
 			black: 'font-black',
+			extrabold: 'font-extrabold',
+			bold: 'font-bold',
+			semibold: 'font-semibold',
+			medium: 'font-medium',
+			normal: 'font-normal',
+			light: 'font-light',
+			extralight: 'font-extralight',
+			thin: 'font-thin',
 		},
 		align: {
 			left: 'text-left',
@@ -36,15 +46,16 @@ type HeadingProps = { as?: HeadingType } & VariantProps<typeof headingVariants> 
  *
  * @param {HeadingType} [as='h1'] - The HTML heading element type (h1, h2, h3, h4, h5, or h6).
  * @param {string} [weight='bold'] - The font weight of the heading (thin, extralight, light, normal, medium, semibold, bold, extrabold, or black).
+ * @param {string} [size='4xl'] - The size variant of the heading (4xl, 3xl, 2xl, xl, lg, md, sm, xs).
  * @param {string} [align='left'] - The text alignment of the heading (left, center, or right).
  * @param {ReactNode} children - The content to be displayed within the heading.
  * @param {string} className - Additional CSS classes to be applied to the heading.
  * @param {HTMLAttributes<HTMLHeadingElement>} props - Additional HTML attributes to be passed to the underlying heading element.
  */
-const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(({ as = 'h1', weight = 'bold', align = 'left', children, className, ...props }, ref) => {
+const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(({ as = 'h1', size = '4xl', weight = 'bold', align = 'left', children, className, ...props }, ref) => {
 	const Comp = as
 	return (
-		<Comp ref={ref} className={cn(headingVariants({ weight, align }), className)} {...props}>
+		<Comp ref={ref} className={cn(headingVariants({ size, weight, align }), className)} {...props}>
 			{children}
 		</Comp>
 	)
