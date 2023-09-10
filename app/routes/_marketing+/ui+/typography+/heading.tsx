c16ac1050945384ecddf013ad7f954a67648fc8f
André Casal <andre@andrecasal.com>
2023-09-10 14:49:09 +0100

    read component source from disk

--- app/routes/_marketing+/ui+/typography+/heading.tsx
+++ app/routes/_marketing+/ui+/typography+/heading.tsx
@@ -1,72 +1,22 @@
+import { json } from '@remix-run/node'
 import { Heading } from '../components/typography/heading.tsx'
 import { CodeBlock } from '~/components/ui/code-block.tsx'
+import { promises as fs } from 'fs'
+import { fileURLToPath } from 'url'
+import path, { dirname } from 'path'
+import { useLoaderData } from '@remix-run/react'
+
+export const loader = async () => {
+	const basePath = '../app/routes/'
+	const __filename = fileURLToPath(import.meta.url)
+	const __dirname = dirname(__filename)
+	const filePath = path.join(__dirname, basePath, '/_marketing+/ui+/components/typography/heading.tsx')
+	const source = await fs.readFile(filePath, 'utf-8')
+	return json({ source })
+}
 
 const HeadingRoute = () => {
-	const headingSource = `import { forwardRef, type ReactNode, type HTMLAttributes } from 'react'
-import { type VariantProps, cva } from 'class-variance-authority'
-import { cn } from '~/utils/misc.ts'
-
-const headingVariants = cva('', {
-	variants: {
-		size: {
-			'4xl': 'text-size-4xl',
-			'3xl': 'text-size-3xl',
-			'2xl': 'text-size-2xl',
-			xl: 'text-size-xl',
-			lg: 'text-size-lg',
-			md: 'text-size-md',
-			sm: 'text-size-sm',
-			xs: 'text-size-xs',
-		},
-		weight: {
-			black: 'font-black',
-			extrabold: 'font-extrabold',
-			bold: 'font-bold',
-			semibold: 'font-semibold',
-			medium: 'font-medium',
-			normal: 'font-normal',
-			light: 'font-light',
-			extralight: 'font-extralight',
-			thin: 'font-thin',
-		},
-		align: {
-			left: 'text-left',
-			center: 'text-center',
-			right: 'text-right',
-		},
-	},
-	defaultVariants: {
-		weight: 'bold',
-		align: 'left',
-	},
-})
-
-type HeadingType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
-
-type HeadingProps = { as?: HeadingType } & VariantProps<typeof headingVariants> & { children: ReactNode } & HTMLAttributes<HTMLHeadingElement>
-
-/**
- * A Heading component.
- *
- * @param {HeadingType} [as='h1'] - The HTML heading element type (h1, h2, h3, h4, h5, or h6).
- * @param {string} [weight='bold'] - The font weight of the heading (thin, extralight, light, normal, medium, semibold, bold, extrabold, or black).
- * @param {string} [size='4xl'] - The size variant of the heading (4xl, 3xl, 2xl, xl, lg, md, sm, xs).
- * @param {string} [align='left'] - The text alignment of the heading (left, center, or right).
- * @param {ReactNode} children - The content to be displayed within the heading.
- * @param {string} className - Additional CSS classes to be applied to the heading.
- * @param {HTMLAttributes<HTMLHeadingElement>} props - Additional HTML attributes to be passed to the underlying heading element.
- */
-const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(({ as = 'h1', size = '4xl', weight = 'bold', align = 'left', children, className, ...props }, ref) => {
-	const Comp = as
-	return (
-		<Comp ref={ref} className={cn(headingVariants({ size, weight, align }), className)} {...props}>
-			{children}
-		</Comp>
-	)
-})
-Heading.displayName = 'Heading'
-
-export { Heading }`
+	const { source } = useLoaderData<typeof loader>()
 
 	return (
 		<>
@@ -78,7 +28,7 @@ export { Heading }`
 			<Heading as="h2" size="xl" className="mt-8">
 				Source
 			</Heading>
-			<CodeBlock code={headingSource} filename="heading" extension="tsx" className="mt-4" />
+			<CodeBlock code={source} filename="heading" extension="tsx" className="mt-4" />
 
 			<Heading as="h2" size="xl" className="mt-8">
 				Usage Example
