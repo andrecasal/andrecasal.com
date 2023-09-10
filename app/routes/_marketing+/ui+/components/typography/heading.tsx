5761fabf479a0719dc94ab5fe620c39698d0a9d7
André Casal <andre@andrecasal.com>
2023-09-10 14:48:59 +0100

    moved defaults to component props only

--- app/routes/_marketing+/ui+/components/typography/heading.tsx
+++ app/routes/_marketing+/ui+/components/typography/heading.tsx
@@ -31,10 +31,6 @@ const headingVariants = cva('', {
 			right: 'text-right',
 		},
 	},
-	defaultVariants: {
-		weight: 'bold',
-		align: 'left',
-	},
 })
 
 type HeadingType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
