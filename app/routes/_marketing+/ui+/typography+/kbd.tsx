feb51c1dad9e73e36fe39fc312b8f007298fb119
André Casal <andre@andrecasal.com>
2023-09-26 09:01:39 +0100

    add styling to kbd documentation

--- app/routes/_marketing+/ui+/typography+/kbd.tsx
+++ app/routes/_marketing+/ui+/typography+/kbd.tsx
@@ -20,6 +20,21 @@ export const loader = async () => {
 const HeadingRoute = () => {
 	const { source } = useLoaderData<typeof loader>()
 	const usageSource = `<p>Press <Kbd>Shift + Left</Kbd>, <Kbd>Shift + Right</Kbd>, <Kbd>Shift + Up</Kbd>, <Kbd>Shift + Down</Kbd> to move things by 10px.</p>`
+	const stylingSource = `.kbd {
+	margin: var(--space-0);
+	padding: var(--space-4);
+	border-color: hsl(var(--color-muted-200));
+	border-width: var(--space-1);
+	border-radius: var(--radius);
+	font-family: var(--font-code);
+	font-size: var(--text-size-xs);
+	line-height: var(--text-line-height-xs);
+	letter-spacing: var(--text-tracking-xs);
+	background-color: hsl(var(--color-muted-50));
+	color: hsl(var(--color-muted-900));
+	box-shadow: var(--space-0) var(--space-1) var(--space-2) var(--space-0) hsl(var(--color-foreground) / 0.05);
+	vertical-align: middle;
+}`
 
 	return (
 		<>
@@ -28,7 +43,7 @@ const HeadingRoute = () => {
 			</Heading>
 			<Text className="mt-4">Marks text as keyboard or voice (or another entry device) input.</Text>
 			<Text className="mt-4">
-				This component is merely a styling layer on top of the <code>{'<kbr>'}</code> tag.
+				This component is merely a styling layer on top of the <code>{'<kbd>'}</code> tag.
 			</Text>
 			<Heading as="h2" size="3xl" className="mt-8">
 				Usage
@@ -43,6 +58,11 @@ const HeadingRoute = () => {
 				Source
 			</Heading>
 			<CodeBlock code={source} filename="kbd" extension="tsx" className="mt-4" />
+
+			<Heading as="h2" size="3xl" className="mt-8">
+				Styling
+			</Heading>
+			<CodeBlock code={stylingSource} filename="tailwind" extension="css" className="mt-4" />
 		</>
 	)
 }
