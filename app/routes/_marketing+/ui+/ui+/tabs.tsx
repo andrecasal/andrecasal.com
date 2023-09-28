2a449e700376f74b995eaf23d5ba03c443990a7c
André Casal <andre@andrecasal.com>
2023-09-28 13:13:23 +0100

    removed props (for now)

--- app/routes/_marketing+/ui+/ui+/tabs.tsx
+++ app/routes/_marketing+/ui+/ui+/tabs.tsx
@@ -161,84 +161,10 @@ const HeadingRoute = () => {
 				</div>
 			</Tabs.Root>
 
-			<Heading as="h2" size="xl" className="mt-8">
-				Props
-			</Heading>
-			<div className="mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
-				<table className="min-w-full divide-y divide-gray-300">
-					<thead className="bg-gray-50">
-						<tr>
-							<th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-								Prop
-							</th>
-							<th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-								Type
-							</th>
-							<th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-								Default
-							</th>
-						</tr>
-					</thead>
-					<tbody className="divide-y divide-gray-200 bg-white">
-						<tr>
-							<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">inline</td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">boolean</td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">false</td>
-						</tr>
-						<tr>
-							<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">direction</td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">'row' | 'col'</td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">'row'</td>
-						</tr>
-						<tr>
-							<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">justify</td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">'start' | 'center' | 'end' | 'between' </td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">'start'</td>
-						</tr>
-						<tr>
-							<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">align</td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">'start' | 'center' | 'end' | 'baseline' | 'stretch'</td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">'start'</td>
-						</tr>
-						<tr>
-							<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">gap</td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">'0' ... '20'</td>
-							<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">'0'</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-
-			<Heading as="h2" size="3xl" className="mt-8">
-				Notes
-			</Heading>
-			<Text className="mt-4">
-				If you set <code>inline</code>, justify has no meaning because the <code>{'<Flex />'}</code> has the least width possible.
-			</Text>
-			<Text className="mt-4">
-				If you set <code>align="stretch"</code>, the children shouldn't have a fixed height, because otherwise, they won't stretch.
-			</Text>
-			<Text className="mt-4">
-				Flexbox loses a lot of its power if you use <code>flex-wrap: wrap</code>, so this option is omitted on purpose. If you need to wrap, you should probably use a grid instead.
-			</Text>
-
-			<Heading as="h2" size="3xl" className="mt-8">
-				Accessibility
-			</Heading>
-			<Text className="mt-4">
-				Changing visual order creates a disconnect between content and presentation and is, therefore, bad for accessibility. For this reason, I've purposely ommitted the following
-				option:
-			</Text>
-			<ul className="ml-4 mt-2 list-disc">
-				<li>
-					<code>flow</code> does not support <code>row-dense</code> and <code>col-dense</code> options.
-				</li>
-			</ul>
-
 			<Heading as="h2" size="3xl" className="mt-8">
 				Source
 			</Heading>
-			<CodeBlock code={source} filename="flex" extension="tsx" className="mt-4" />
+			<CodeBlock code={source} filename="tabs" extension="tsx" className="mt-4" />
 		</>
 	)
 }
