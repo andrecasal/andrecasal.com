5d2096e8e3d4d40830fd93cb1cb817bb82ef6dcf
André Casal <andre@andrecasal.com>
2023-11-16 01:07:48 +0000

    removed unused import

--- app/routes/_marketing+/ui+/components/typography/q.tsx
+++ app/routes/_marketing+/ui+/components/typography/q.tsx
@@ -1,5 +1,5 @@
+import { forwardRef, type HTMLAttributes } from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
-import { forwardRef, type HTMLAttributes, type ReactNode } from 'react'
 import { cn } from '~/utils/tailwind-merge.ts'
 
 const quoteVariants = cva('', {
