6a1c3ca4dbd57dd6293cb0c65d39739d3062a786
André Casal <andre@andrecasal.com>
2024-02-09 11:22:20 +0000

    Add cn utility function to code component

--- app/routes/_marketing+/ui+/components/typography/code.tsx
+++ app/routes/_marketing+/ui+/components/typography/code.tsx
@@ -1,6 +1,7 @@
 import { forwardRef, useState, type HTMLAttributes, type KeyboardEvent } from 'react'
 import { useCopyToClipboard } from 'usehooks-ts'
 import { Tooltip } from '~/routes/_marketing+/ui+/components/ui/tooltip.tsx'
+import { cn } from '~/utils/tailwind-merge.ts'
 
 type CodeProps = { copy?: boolean } & { children: string } & HTMLAttributes<HTMLElement>
 
@@ -36,12 +37,12 @@ const Code = forwardRef<HTMLElement, CodeProps>(({ copy = false, children, class
 
 	return copy ? (
 		<Tooltip key={children} content={showCopyFeedback ? 'Click to copy to clipboard' : 'Saved to clipboard!'}>
-			<code ref={ref} className={`code ${copy ? 'cursor-pointer' : ''}`} {...props} onClick={() => handleClick(children)} onKeyDown={e => handleKeyDown(e, children)}>
+			<code ref={ref} className={cn(`code ${copy ? 'cursor-pointer' : ''}`, className)} {...props} onClick={() => handleClick(children)} onKeyDown={e => handleKeyDown(e, children)}>
 				{children}
 			</code>
 		</Tooltip>
 	) : (
-		<code ref={ref} className="code" {...props}>
+		<code ref={ref} className={cn('code', className)} {...props}>
 			{children}
 		</code>
 	)
